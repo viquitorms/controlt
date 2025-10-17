5f31c06e17e4919ecdfbba22e1b693a657151b45
Victor Magalhães <victor.souza@logisticmobile.com>
2025-10-17 14:28:56 -0300

    fixing and adjusting code

--- controlt.backend/src/routes/team.route.js
+++ controlt.backend/src/routes/team.route.js
@@ -4,17 +4,14 @@ import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
-// Todas as rotas protegidas
 router.use(authMiddleware);
 
-// CRUD básico
 router.post('/', teamController.create);
 router.get('/', teamController.list);
 router.get('/:id', teamController.findById);
 router.put('/:id', teamController.update);
 router.delete('/:id', teamController.delete);
 
-// Gestão de membros
 router.get('/:id/members', teamController.getMembers);
 router.post('/:id/members', teamController.addMember);
 router.delete('/:id/members/:userId', teamController.removeMember);
